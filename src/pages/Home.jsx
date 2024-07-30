11620728da70b45ba5663d161e5e43d1efae1c05
furkanyucel <furkanyucelux@gmail.com>
2024-07-30 10:14:16 +0300

    useNavigate Kullanımı

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
-  return <h1>Home</h1>;
+
+  const navigate = useNavigate();
+
+  const useGate = () => {
+    navigate("/products");
+  }
+
+  return (
+    <Fragment>
+      <h1>Home</h1>
+      <button onClick={useGate}>Navigate Kullan</button>
+    </Fragment>
+  )
 }
 
 export default Home;
